b289a976ca2aef1ef595464d578c7dac0000ea86
Celine M.O. Reisser <celine.reisser@gmail.com>
2018-12-14 10:46:32 -1000

    Update 02_gatk_splitNcigar.sh

--- 00_scripts/02_gatk_splitNcigar.sh
+++ 00_scripts/02_gatk_splitNcigar.sh
@@ -1,20 +1,21 @@
-SCRIPT=/home1/datawork/creisser/GAMMA/genome_based/00_scripts/02_gatk
-HEADER=/home1/datawork/creisser/GAMMA/genome_based/00_scripts/header-big-mem.txt
+WORKDIR="/home1/datawork/creisser/daphnia_snp"
+SCRIPT=00_scripts/02_gatk
+HEADER=00_scripts/header-big-mem.txt
 GATK="/home1/datahome/creisser/local-programs/GenomeAnalysisTK.jar"
 NAME='cat /home1/datawork/creisser/GAMMA/genome_based/00_scripts/base.txt'
-DATADIRECTORY=/home1/scratch/creisser/GAMMA/genome/01_MD
-DATAOUT=/home1/scratch/creisser/GAMMA/genome/02_Ncigar
+DATADIRECTORY=03_MD
+DATAOUT=04_Ncigar
 SAMTOOLS=". /appli/bioinfo/samtools/latest/env.sh" # samtools-1.4.1
-ASSEMBLY="/home1/datawork/creisser/GAMMA/genome_based/01_info_files/sspace.final.scaffolds"
+ASSEMBLY="01_info_files/sspace.final.scaffolds"
 mkdir -p $SCRIPT
 mkdir -p $DATAOUT
 
-
+cd $WORKDIR
 for FILE in $($NAME)
 do
-        cp $HEADER $SCRIPT/samtools_${FILE##*/}.qsub ;
-        echo "cd $DATADIRECTORY" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "java -Xmx23G -jar $GATK -T SplitNCigarReads -R "$ASSEMBLY".fasta -I $DATADIRECTORY/"$FILE".concordant_uniq.sorted_MD.bam -o $DATAOUT/"$FILE".concordant_uniq.sorted_MD_split.bam ;" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	qsub $SCRIPT/samtools_${FILE##*/}.qsub ;
+        cp $HEADER $SCRIPT/Ncigar_${FILE##*/}.qsub ;
+        echo "cd $WORKDIR/$DATADIRECTORY" >> $SCRIPT/Ncigar_${FILE##*/}.qsub ;
+	echo "java -Xmx23G -jar $GATK -T SplitNCigarReads -R "$ASSEMBLY".fasta -I "$FILE".concordant_uniq.sorted_MD.bam -o $WORKDIR/$DATAOUT/"$FILE".concordant_uniq.sorted_MD_split.bam ;" >> $SCRIPT/Ncigar_${FILE##*/}.qsub ;
+	qsub $SCRIPT/Ncigar_${FILE##*/}.qsub ;
 done ;
 
