35ab39c9d52e293b619b342cb7020592704e722a
Celine M.O. Reisser <celine.reisser@gmail.com>
2018-12-14 11:07:35 -1000

    Update 03_samtools_filter_sort.sh

--- 00_scripts/03_samtools_filter_sort.sh
+++ 00_scripts/03_samtools_filter_sort.sh
@@ -5,6 +5,7 @@ LOG_FOLDER=98_log_files
 NAME='cat /home1/datawork/creisser/daphnia_snp/00_scripts/base.txt'
 SCRIPT=00_scripts/03_samtools_filter
 HEADER=00_scripts/header.txt
+OLD=03_MD
 
 $WORDIR
 
@@ -16,11 +17,8 @@ do
 	cp $WORDIR/$HEADER $WORDIR/$SCRIPT/samtools_${FILE##*/}.qsub ;
 	echo "cd $WORDIR/$INDIR" >> $WORDIR/$SCRIPT/samtools_${FILE##*/}.qsub ;
 	echo "$SAMTOOLSENV"  >> $WORDIR/$SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "samtools view
-	echo "samtools view -b -F4 -F 256 -q5 "$FILE".sam -o "$FILE".bam"  >> $WORDIR/$SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "samtools sort -@ 8 "$FILE".bam -o "$FILE".sorted.bam"  >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "samtools index -b $OUTDIR/"$FILE".sorted.bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ; 
-	echo "rm $OUTDIR/"$FILE".bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
+	echo "samtools index -b "$FILE".sorted_MD_split.bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ; 
+	echo "rm $WORDIR/$OLD/"$FILE".sorted_MD.bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
 	qsub $SCRIPT/samtools_${FILE##*/}.qsub ;
 done ;
 
