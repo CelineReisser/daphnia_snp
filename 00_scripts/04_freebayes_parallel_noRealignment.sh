f9e7c2aec3008c12d34c535fdd69cb6d99d0cbf3
Celine M.O. Reisser <celine.reisser@gmail.com>
2018-12-14 11:22:22 -1000

    Update 04_freebayes_parallel_noRealignment.sh

--- 00_scripts/04_freebayes_parallel_noRealignment.sh
+++ 00_scripts/04_freebayes_parallel_noRealignment.sh
@@ -16,15 +16,16 @@ LS="ls $WORKDIR/$DATADIRECTORY/*.sorted_MD_split.bam"
 $LS > 00_scripts/bam_list_freebayes.txt
 BAM=00_scripts/bam_list_freebayes.txt
 
-NCPU=50
-nAlleles=4
-minMapQ=30
-minCOV=10
-
-$FREEBAYESENV
-
-freebayes-parallel <(fasta_generate_regions.py $WORKDIR/$INDEX 100000) "$NCPU" \
--f $REF --use-best-n-alleles $nAlleles -C 5 --min-mapping-quality $minMapQ --min-coverage $minCOV --genotype-qualities -L $BAM > $WORKDIR/$OUTDIR/daphnia_freebayes_parallel.vcf
+do
+cp $WORDIR/$HEADER $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "NCPU=50" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "nAlleles=4" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "minMapQ=30" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "minCOV=10" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "$FREEBAYESENV" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+echo "freebayes-parallel <(fasta_generate_regions.py $WORKDIR/$INDEX 100000) "$NCPU" -f $REF --use-best-n-alleles $nAlleles -C 5 --min-mapping-quality $minMapQ --min-coverage $minCOV --genotype-qualities -L $BAM > $WORKDIR/$OUTDIR/daphnia_freebayes_parallel.vcf" >> $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+qsub $WORDIR/00_scripts/04_freebayes_parallel_noRealignment.qsub ;
+done ;
 
 
 
