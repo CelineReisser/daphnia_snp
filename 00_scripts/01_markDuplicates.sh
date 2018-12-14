ac49370a7c8c2013a888d36741db3f04aaa88182
Celine M.O. Reisser <celine.reisser@gmail.com>
2018-12-14 10:52:20 -1000

    Update 01_markDuplicates.sh

--- 00_scripts/01_markDuplicates.sh
+++ 00_scripts/01_markDuplicates.sh
@@ -1,6 +1,6 @@
 WORKDIR="/home1/datawork/creisser/daphnia_snp"
 SCRIPT=00_scripts/01_markDuplicates
-HEADER=00_scripts/header-big-mem.txt
+HEADER=00_scripts/header.txt
 PICARDTOOLS="/home1/datahome/creisser/local-programs/picard-tools-1.119"
 NAME='cat 00_scripts/base.txt'
 DATADIRECTORY=02_data
@@ -14,7 +14,7 @@ for FILE in $($NAME)
 do
         cp $WORKDIR/$HEADER $WORKDIR/$SCRIPT/picard_${FILE##*/}.qsub ;
         echo "cd $WORKDIR/$DATADIRECTORY" >> $WORKDIR/$SCRIPT/picard_${FILE##*/}.qsub ;
-        echo "java -Xmx23G -jar ${PICARDTOOLS}/MarkDuplicates.jar I=Aligned.sortedByCoord"$FILE".out.bam O=$WORKDIR/$DATAOUT/"$FILE".sorted_MD.bam M=$WORKDIR/$DATAOUT/"$FILE".MarkDup_metrics.txt ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=TRUE CREATE_INDEX=TRUE" >> $WORKDIR/$SCRIPT/picard_${FILE##*/}.qsub ;
+        echo "java -Xmx60G -jar ${PICARDTOOLS}/MarkDuplicates.jar I=Aligned.sortedByCoord"$FILE".out.bam O=$WORKDIR/$DATAOUT/"$FILE".sorted_MD.bam M=$WORKDIR/$DATAOUT/"$FILE".MarkDup_metrics.txt ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=TRUE CREATE_INDEX=TRUE" >> $WORKDIR/$SCRIPT/picard_${FILE##*/}.qsub ;
         qsub $WORKDIR/$SCRIPT/picard_${FILE##*/}.qsub;
 done;
 
