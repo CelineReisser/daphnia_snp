ad20c6747bf7c30094b3da7c788632ab35db99d0
Celine M.O. Reisser <celine.reisser@gmail.com>
2018-12-14 10:49:57 -1000

    Update 02_gatk_splitNcigar.sh

--- 00_scripts/02_gatk_splitNcigar.sh
+++ 00_scripts/02_gatk_splitNcigar.sh
@@ -13,9 +13,9 @@ mkdir -p $DATAOUT
 cd $WORKDIR
 for FILE in $($NAME)
 do
-        cp $HEADER $SCRIPT/Ncigar_${FILE##*/}.qsub ;
-        echo "cd $WORKDIR/$DATADIRECTORY" >> $SCRIPT/Ncigar_${FILE##*/}.qsub ;
-	echo "java -Xmx23G -jar $GATK -T SplitNCigarReads -R "$ASSEMBLY".fasta -I "$FILE".concordant_uniq.sorted_MD.bam -o $WORKDIR/$DATAOUT/"$FILE".concordant_uniq.sorted_MD_split.bam ;" >> $SCRIPT/Ncigar_${FILE##*/}.qsub ;
-	qsub $SCRIPT/Ncigar_${FILE##*/}.qsub ;
+        cp $WORKDIR/$HEADER $WORKDIR/$SCRIPT/Ncigar_${FILE##*/}.qsub ;
+        echo "cd $WORKDIR/$DATADIRECTORY" >> $WORKDIR/$SCRIPT/Ncigar_${FILE##*/}.qsub ;
+	echo "java -Xmx60G -jar $GATK -T SplitNCigarReads -R "$ASSEMBLY".fasta -I "$FILE".concordant_uniq.sorted_MD.bam -o $WORKDIR/$DATAOUT/"$FILE".concordant_uniq.sorted_MD_split.bam ;" >> $WORKDIR/$SCRIPT/Ncigar_${FILE##*/}.qsub ;
+	qsub $WORKDIR/$SCRIPT/Ncigar_${FILE##*/}.qsub ;
 done ;
 
